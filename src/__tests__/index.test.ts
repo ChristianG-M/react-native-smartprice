2a1a2840710bdab67288e1bf476f6f60d6840e4d
Will Hwang <will@prescryptive.com>
2021-03-24 14:41:42 +0900

    writing pipeline

--- src/__tests__/index.test.ts
+++
@@ -1,6 +0,0 @@
-// Copyright 2021 Prescryptive Health, Inc.
-import { addOne } from '../add';
-
-test('addOne', () => {
-  expect(addOne(0)).toEqual(1);
-});
--- src/add.ts
+++
@@ -1,2 +0,0 @@
-// Copyright 2021 Prescryptive Health, Inc.
-export const addOne = (input: number): number => input + 1;
