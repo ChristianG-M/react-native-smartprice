489c5c570e2e5115dde99efe8d797370b77d0151
v-alex <v-alex@prescryptive.com>
2021-03-25 13:51:48 -0500

    index

--- example/src/component/inputs/mask-inputs/date-mask-input/date-mask-input.content.es.test.ts
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 Prescryptive Health, Inc.
-
-import { dateMaskInputContentEs } from './date-mask-input.content.es';
-
-describe('DateMaskInputContent', () => {
-  it('has expected content (default)', () => {
-    expect(dateMaskInputContentEs.placeholder).toEqual('Fecha de nacimiento');
-  });
-});
--- example/src/component/inputs/mask-inputs/phone-mask-input/phone-mask-input.content.es.test.ts
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 Prescryptive Health, Inc.
-
-import { phoneMaskInputContentEs } from './phone-mask-input.content.es';
-
-describe('PhoneMaskInputContent', () => {
-  it('has expected content (default)', () => {
-    expect(phoneMaskInputContentEs.placeholder).toEqual('Número telefónico');
-  });
-});
